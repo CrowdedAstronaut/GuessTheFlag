a8b0124b7be2d272ead80fa5efd6373121e13d11
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-14 10:07:34 -0500

    Using stacks to arrange views

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, Ross!")
-            .padding()
-    }
+	var body: some View {
+		ZStack {
+			Text("Hello, world!")
+			Text("This is inside a stack")
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
