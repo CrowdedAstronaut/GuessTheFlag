8a65d44ff8253cf78ccc5ebe4bb949e5ae8eb35a
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-14 09:57:42 -0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ross Phillips on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
