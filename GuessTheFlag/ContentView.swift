c16670276ac3cc3c98afe31d8a72944538515c04
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-14 10:10:29 -0500

    Created 3x3 grid.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
 	var body: some View {
-		ZStack {
-			Text("Hello, world!")
-			Text("This is inside a stack")
+		VStack {
+			HStack(spacing: 20) {
+				Text("1")
+				Text("2")
+				Text("3")
+			}
+			HStack(spacing: 20) {
+				Text("1")
+				Text("2")
+				Text("3")
+			}
+			HStack(spacing: 20) {
+				Text("1")
+				Text("2")
+				Text("3")
+			}
 		}
 	}
 }
