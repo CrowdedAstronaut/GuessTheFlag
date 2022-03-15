e820b34dcc561c61824b38226d6ae50404b1a99a
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-15 10:31:22 -0500

    Upgrading our design

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -15,15 +15,24 @@ struct ContentView: View {
 	
 	var body: some View {
 		ZStack{
-			LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+			RadialGradient(stops: [
+				.init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+				.init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+			], center: .top, startRadius: 200, endRadius: 400)
 				.ignoresSafeArea()
-			VStack(spacing: 30){
+			.ignoresSafeArea()
+			VStack{
+				Spacer()
+				Text("Guess the Flag")
+					   .font(.largeTitle.weight(.bold))
+					   .foregroundColor(.white)
+			VStack(spacing: 15){
 				VStack{
 					Text("Tap the flag of")
-						.foregroundColor(.white)
+						.foregroundStyle(.secondary)
 						.font(.subheadline.weight(.heavy))
 					Text(countries[correctAnswer])
-						.foregroundColor(.white)
+		
 					.font(.largeTitle.weight(.semibold))				}
 				
 				ForEach(0..<3){ number in
@@ -37,6 +46,21 @@ struct ContentView: View {
 					}
 				}
 			}
+			.frame(maxWidth: .infinity)
+			.padding(.vertical, 20)
+			.background(.regularMaterial)
+			.clipShape(RoundedRectangle(cornerRadius: 20))
+				
+				Spacer()
+				Spacer()
+				Text("Score: ???")
+					.foregroundColor(.white)
+					.font(.title.bold())
+				Spacer()
+
+			}
+			.padding()
+
 		}
 		.alert(scoreTitle, isPresented: $showingScore) {
 			Button("Continue", action: askQuestion)
@@ -50,7 +74,7 @@ struct ContentView: View {
 		} else {
 			scoreTitle = "Wrong"
 		}
-
+		
 		showingScore = true
 	}
 	func askQuestion() {
