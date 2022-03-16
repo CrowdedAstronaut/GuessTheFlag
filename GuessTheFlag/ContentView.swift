319d0767aeb0021c146328192cce7c9ce2c70db6
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-16 09:56:28 -0500

    Challenge 1: Score tracking

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
 	@State private var scoreTitle = ""
 	@State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
 	@State private var correctAnswer = Int.random(in: 0...2)
+	@State private var score = 0
 	
 	var body: some View {
 		ZStack{
@@ -53,7 +54,7 @@ struct ContentView: View {
 				
 				Spacer()
 				Spacer()
-				Text("Score: ???")
+				Text("Score: \(score)")
 					.foregroundColor(.white)
 					.font(.title.bold())
 				Spacer()
@@ -65,14 +66,19 @@ struct ContentView: View {
 		.alert(scoreTitle, isPresented: $showingScore) {
 			Button("Continue", action: askQuestion)
 		} message: {
-			Text("Your score is ???")
+			Text("Your score is \(score)")
 		}
 	}
 	func flagTapped(_ number: Int) {
 		if number == correctAnswer {
 			scoreTitle = "Correct"
+			score += 1
 		} else {
 			scoreTitle = "Wrong"
+			if score > 0{
+				score -= 1
+			}
+		
 		}
 		
 		showingScore = true
