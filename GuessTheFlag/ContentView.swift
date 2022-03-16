bbedb9ac1be9014dc1319b0519cb13153fb59735
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-16 10:10:28 -0500

    Guess the Flag - Challenge 2 - learning from errors

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -20,37 +20,37 @@ struct ContentView: View {
 				.init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
 				.init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
 			], center: .top, startRadius: 200, endRadius: 400)
-				.ignoresSafeArea()
+			.ignoresSafeArea()
 			.ignoresSafeArea()
 			VStack{
 				Spacer()
 				Text("Guess the Flag")
-					   .font(.largeTitle.weight(.bold))
-					   .foregroundColor(.white)
-			VStack(spacing: 15){
-				VStack{
-					Text("Tap the flag of")
-						.foregroundStyle(.secondary)
-						.font(.subheadline.weight(.heavy))
-					Text(countries[correctAnswer])
-		
-					.font(.largeTitle.weight(.semibold))				}
-				
-				ForEach(0..<3){ number in
-					Button{
-						flagTapped(number)
-					} label: {
-						Image(countries[number])
-							.renderingMode(.original)
-							.clipShape(Capsule())
-							.shadow(radius: 5)
+					.font(.largeTitle.weight(.bold))
+					.foregroundColor(.white)
+				VStack(spacing: 15){
+					VStack{
+						Text("Tap the flag of")
+							.foregroundStyle(.secondary)
+							.font(.subheadline.weight(.heavy))
+						Text(countries[correctAnswer])
+						
+						.font(.largeTitle.weight(.semibold))				}
+					
+					ForEach(0..<3){ number in
+						Button{
+							flagTapped(number)
+						} label: {
+							Image(countries[number])
+								.renderingMode(.original)
+								.clipShape(Capsule())
+								.shadow(radius: 5)
+						}
 					}
 				}
-			}
-			.frame(maxWidth: .infinity)
-			.padding(.vertical, 20)
-			.background(.regularMaterial)
-			.clipShape(RoundedRectangle(cornerRadius: 20))
+				.frame(maxWidth: .infinity)
+				.padding(.vertical, 20)
+				.background(.regularMaterial)
+				.clipShape(RoundedRectangle(cornerRadius: 20))
 				
 				Spacer()
 				Spacer()
@@ -58,10 +58,10 @@ struct ContentView: View {
 					.foregroundColor(.white)
 					.font(.title.bold())
 				Spacer()
-
+				
 			}
 			.padding()
-
+			
 		}
 		.alert(scoreTitle, isPresented: $showingScore) {
 			Button("Continue", action: askQuestion)
@@ -74,11 +74,19 @@ struct ContentView: View {
 			scoreTitle = "Correct"
 			score += 1
 		} else {
-			scoreTitle = "Wrong"
+			let needsThe = ["UK", "US"]
+			let theirAnswer = (countries[number])
+			
+			if needsThe.contains(theirAnswer){
+				scoreTitle = "Wrong That's the flag of the \(theirAnswer)"
+			} else {
+				scoreTitle = "Wrong That's the flag of \(theirAnswer)"
+			}
+			
 			if score > 0{
 				score -= 1
 			}
-		
+			
 		}
 		
 		showingScore = true
